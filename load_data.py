ba235f9f8938479797d54983f4497a0a5479a69b
Arthur Deng <arthurdeng0205@hotmail.com>
2021-02-25 23:57:58 +0800

    worked on basic data structure (vector conversion)

--- load_data.py
+++ load_data.py
@@ -17,10 +17,33 @@ def initialize():
     # m = 60000
     # trainImg is a 60000 x 28 x 28 array; contains x(i) (28 x 28 matrix)
     # trainLabel is a 60000 x 1 array; contains y(i) | {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-    trainImg = convert_to_numpy('Data/train-img') 
-    trainLabel = convert_to_numpy('Data/train-label') 
-    testImg = convert_to_numpy('Data/test-img')
-    testLabel = convert_to_numpy('Data/test-label')
-    return trainImg, trainLabel, testImg, testLabel
+
+    # read from file
+    train_img_raw = convert_to_numpy('Data/train-img')
+    test_img_raw = convert_to_numpy('Data/test-img')
+    train_result = convert_to_numpy('Data/train-label') 
+    test_result = convert_to_numpy('Data/test-label')
+
+    train_len, test_len, dim = len(train_img_raw), len(test_img_raw), 28
+
+    # x(i) is a 28^2 dimension vector
+    train_img = [train_img_raw[i].reshape([1, dim**2]) for i in range(train_len)]
+    test_img = [test_img_raw[i].reshape([1, dim**2]) for i in range(test_len)]
+
+    # y(i) is a 10 dimension vector
+    train_label = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0] for i in range(train_len)]
+    test_label = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0] for i in range(test_len)]
+    for i in range(train_len):
+        train_label[i][train_result[i]] = 1
+    for i in range(test_len):
+        test_label[i][test_result[i]] = 1
+
+    # splitting test set to 50% test and 50% cross validation
+    CV_label, CV_img = test_label[test_len//2:], test_img[test_len//2:]
+    test_label, test_img = test_label[:test_len//2], test_img[:test_len//2]
+
+    #returns three sets: train, CV, and test
+    return train_img, train_label, CV_img, CV_label, test_img, test_label
 
 
+initialize()
