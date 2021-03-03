7260c5ca8df2e2380d63c9bfed2fcc05ce5bdccf
Arthur Deng <arthurdeng0205@hotmail.com>
2021-03-04 00:06:08 +0800

    figured out what's wrong.. trying new things tmrw

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ train_img, train_label = load_data.initialize()
 
 n = 785
 m = 60000
-m_dev = 5 #smaller number of training data to check functionality of program
+m_dev = 10 #smaller number of training data to check functionality of program
 lam = 0 #lambda -- regularization parameter (Bias/Variance tradeoff)
 L = 3 # number of layers: layer 0, layer 1, layer 2
 
@@ -38,19 +38,23 @@ def gradient_checking(Theta, X = train_img, Y = train_label):
     """ gradient checking
         uses X[0] and Y[0] by default for approximation
     """
-    epsilon, D = 0.01, backprop(Theta)
+    epsilon, D = 1000, backprop(Theta)
     D_check = [np.zeros((n-1, n)), np.zeros((10, n))]
     for l in range(L - 2):
+        # print("check")
         row, col = D_check[l].shape[0], D_check[l].shape[1]
-        for i in range(row//10):
-            for j in range(col//10):    
-                thetaMinus, thetaPlus = Theta, Theta
+        for i in range(row):
+            for j in range(col):    
+                thetaMinus, thetaPlus = Theta[:], Theta[:]
                 thetaMinus[l][i][j] -= epsilon
                 thetaPlus[l][i][j] += epsilon
+                # print(thetaMinus[l][i][j])
+                # print(thetaPlus == Theta)
                 D_check[l][i][j] = (J(thetaPlus, X[0], Y[0]) - J(thetaMinus, X[0], Y[0])) / (2 * epsilon)
-    print(D[0])
-    print(D_check[0])
+    # plt.matshow(D[1].reshape((50, 157)))
     
+    # plt.matshow(D_check[1].reshape((50, 157)))
+    # plt.show()
 ### Tranfer Matrices with randomly initialized weights
 ### Theta[j] maps layer j to layer j + 1
 Theta = [np.zeros((n-1, n)), np.zeros((10, n))]
@@ -101,9 +105,6 @@ def backprop(Theta, X = train_img, Y = train_label):
             if l < L - 2:
                 mtx = mtx[1:, :]
             DELTA[l] = DELTA[l] + mtx
-            print("========================")
-            print(DELTA[l])
-            print("========================")
     for l in range(L - 1):
         row, col = DELTA[l].shape[0], DELTA[l].shape[1]
         for i in range(row):
@@ -118,10 +119,17 @@ def backprop(Theta, X = train_img, Y = train_label):
         
 # gradient_checking(Theta)
 
-for i in range(m_dev):
-    a = forward_prop(Theta, train_img[i], train_label[i])
-    print(a[L-1])
-    print(J(Theta, train_img[i], train_label[i]))
-print(backprop(Theta))
+print(Theta)
+check = Theta[:]
+check[0][75][100] += 10
+print(check)
+print(J(Theta, train_img[0], train_label[0]))
+print(J(check, train_img[0], train_label[0]))
+# for i in range(m_dev):
+#     a = forward_prop(Theta, train_img[i], train_label[i])
+    # print(a[L-1])
+    # print(J(Theta, train_img[i], train_label[i]))
+# print(backprop(Theta))
+# backprop(Theta)
 
 
--- test.py
+++ test.py
@@ -1,26 +1,34 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
+import load_data
 
-# implementation of gradient descent
+train_img, train_label = load_data.initialize()
 
-cost = []
-def gradient_descent(
-    gradient, start, learn_rate, n_iter=50, tolerance=1e-06
-):
-    vector = start
-    for _ in range(n_iter):
-        diff = -learn_rate * gradient(vector)
-        #check if step is less than minimum
-        if np.all(np.abs(diff) <= tolerance):
-            break
-        vector += diff
-        #plotting
-        cost.append(vector)
-    return vector
-
-print(gradient_descent(gradient = lambda x:2*x, start = 10.0, learn_rate = 0.2))
-
-plt.plot(cost)
-plt.ylabel('some numbers')
+plt.matshow(train_img[1][1:].reshape((28, 28)))
 plt.show()
+print(train_label[1])
+
+# # implementation of gradient descent
+
+# cost = []
+# def gradient_descent(
+#     gradient, start, learn_rate, n_iter=50, tolerance=1e-06
+# ):
+#     vector = start
+#     for _ in range(n_iter):
+#         diff = -learn_rate * gradient(vector)
+#         #check if step is less than minimum
+#         if np.all(np.abs(diff) <= tolerance):
+#             break
+#         vector += diff
+#         #plotting
+#         cost.append(vector)
+#     return vector
+
+# print(gradient_descent(gradient = lambda x:2*x, start = 10.0, learn_rate = 0.2))
+
+# plt.plot(cost)
+# plt.ylabel('some numbers')
+# plt.show()
